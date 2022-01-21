0477d184f8b9f9ee05c14d88ee77710d7f88cd27
Corentin <corentin.bourdeau@epitech.eu>
2022-01-21 15:44:12 +0100

    add features

--- back/graph.py
+++ back/graph.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from matplotlib import pyplot as plt 
+from matplotlib.patches import Circle
+import matplotlib.cm as cm
 
 f = open("data.txt", "r")
 # data = f.read()
@@ -8,6 +10,13 @@ x = []
 y = []
 j = 0
 # print(f.read())
+img = plt.imread('images/stade.jpg')
+
+fig, ax = plt.subplots(1)
+ax.imshow(img)
+ax.axis('off')
+
+i = 0
 for line in f.readlines():
     if j == 0:
         x += [int(line)]
@@ -15,14 +24,33 @@ for line in f.readlines():
     else:
         j=0
         y+= [int(line)]
+        print("x :", x[-1], "y :", -y[-1])
+        # circ = Circle((x[-1]/1920*750, -y[-1]/1080*584), 15, color="#FFA50012")
+        # ax.add_patch(circ)
+        # circ = Circle((x[-1]/1920*750, -y[-1]/1080*584), 7, color="#FFA50032")
+        # ax.add_patch(circ)
+        # circ = Circle((x[-1]/1920*750, -y[-1]/1080*584), 3, color="#FFA50064")
+        # ax.add_patch(circ)
+        circ = Circle((x[-1]/1920*750, -y[-1]/1080*584), 4, color=cm.hsv(i/200))
+        ax.add_patch(circ)
+        # if len(y) >=2:
+        #     plt.plot([x[-2]/1920*750, x[-1]/1920*750], [-y[-2]/1080*584, -y[-1]/1080*584], cm.hsv(i/200))
+    i+=1
+    if i == 200:
+        i = 0
     # print(line)
 
-plt.plot(x,y,"-gs")
 
-print("X : ", x, "\nY : ", y)
+# plt.imshow(img)
+# ax.axis("off")
+plt.show()
 
-plt.title("Nuage de points")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.savefig('points.png')
+# plt.plot(x,y,"-gs")
+
+# print("X : ", x, "\nY : ", y)
+
+# plt.title("Nuage de points")
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.savefig('points.png')
 plt.show()
---
+++ back/possession.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import matplotlib.pylab as plt
+import numpy as np
+# npX = np.asarray(X)
+# npY = np.asarray(Y)
+img = plt.imread('images/stade.jpg')
+plt.imshow(img)
+# // TO-DO
+plt.show()
